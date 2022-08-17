c48a8008c37a3c15c7d1bfe36ff555194c1bc8c1
likithatv2000 <50978973+likithatv2000@users.noreply.github.com>
2022-08-17 20:48:01 +0530

    Add files via upload

---
+++ assignment_day1/assignment_day1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write a C# program to check whether the triangle is Equilateral, Isosceles or Scalene based on the input sides from the user.
+
+namespace assignment_day1
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+
